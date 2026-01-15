c5544228fd24de0e883a0d45b50c5a99083b84f9
musicas <ghkueigamer112@gmail.com>
2026-01-15 20:02:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6514718831-805c8",
+  "appId": "1:916733702638:web:306ad52bf62985fd14deff",
+  "apiKey": "AIzaSyCuN9Es28ArI7juKkzG13L2bX3jgvxs31I",
+  "authDomain": "studio-6514718831-805c8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "916733702638"
+};
